a2c9a0537f5e1cb13a06a72bc95320dcc1611fa1
JamesAngelxx3 <xjamesangelo@gmail.com>
2023-10-13 12:38:02 -0500

    add categories

--- Client/src/pages/Categories/Categories.jsx
+++ Client/src/pages/Categories/Categories.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import UbicationBar from '../../components/categories/UbicationBar'
+import SectionCategories from '../../components/categories/SectionCategories'
+import FilterBar from '../../components/categories/FilterBar'
+import ItemsList from '../../components/categories/ItemsList'
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+    <div className='flex flex-col items-center'>
+      <UbicationBar/>
+      <SectionCategories/>
+      <FilterBar/>
+      <ItemsList/>
+
+    </div>
   )
 }
 
