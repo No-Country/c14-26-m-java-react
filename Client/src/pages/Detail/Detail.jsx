6309197a8d765488a92819dbaea346df4bbec6b8
Dalian Aguirre <danathap@gmail.com>
2023-10-23 10:08:49 -0500

    updates

--- Client/src/pages/Detail/Detail.jsx
+++ Client/src/pages/Detail/Detail.jsx
@@ -1,14 +1,15 @@
 import Navigation from '../../components/navigation/Navigation';
 
 import OtherProducts from '../../components/Detail/OtherProducts';
-//import CardProductDetail from '../../components/Detail/CardProductDetail';
+import CardProductDetail from '../../components/Detail/CardProductDetail'
+
 
 
 const Detail = () => {
   return (
     <div className='flex flex-col ml-20 mr-20'>
         <Navigation />
-        {/* <CardProductDetail /> */}
+        <CardProductDetail />
         <OtherProducts />
     </div>
   )
