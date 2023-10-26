51ea628d23dc666061a465867518121e27c550d4
JamesAngelxx3 <xjamesangelo@gmail.com>
2023-10-26 02:41:16 -0500

    add cart funcional

--- Client/src/components/Detail/CardProductDetail.jsx
+++ Client/src/components/Detail/CardProductDetail.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useContext } from 'react';
 import { Context } from '../../context';
+import Swal from 'sweetalert2';
 
 const CardProductDetail = () => {
-  const { getProductByID, product } = useContext(Context);
+  const { getProductByID, product, cart, addCart } = useContext(Context);
 
   useEffect(() => {
-     
+
     const resetProduct = () => {                       // limpio el estado del producto
       getProductByID(-1);                              // -1 ---> estado inicial vacío
     };
     resetProduct();                                    // Limpio el estado antes de cargar un nuevo detalle
-    scrollTo(0, 0)                                    
+    scrollTo(0, 0)
   }, []);
 
   const renderRating = () => {
@@ -19,6 +20,8 @@ const CardProductDetail = () => {
     }
 
     const roundedRating = Math.round(product.rating.rate);
+
+
 
     return (
       <div className="flex items-center">
@@ -37,6 +40,15 @@ const CardProductDetail = () => {
     );
   };
 
+  const AddCartConfirm = () => {
+    Swal.fire(
+      'buen Trabajo!',
+      'Producto Agregado con exito!',
+      'success'
+    )
+    addCart({...product, cantidad: 1})
+  }
+
   return (
     <div className="container mx-auto h-screen flex flex-col items-center">
       <div className="bg-white rounded-lg p-4 mb-8">
@@ -52,9 +64,11 @@ const CardProductDetail = () => {
                 <h2 className="text-lg">{product.title}</h2>
                 {renderRating()} {/* Llamo a la función para mostrar el rating */}
                 <p className="text-lg my-2 font-bold mb-10">${product.price}</p>
-                <p className="text-sm my-3">Description:<br/>{product.description}</p>
-                <p className="text-sm my-3 border-b border-gray-300 pb-8 mb-5">Category:<br/>{product.category}</p>
-                <button className="bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 mb-5">Add Cart</button>
+                <p className="text-sm my-3">Description:<br />{product.description}</p>
+                <p className="text-sm my-3 border-b border-gray-300 pb-8 mb-5">Category:<br />{product.category}</p>
+                <button className="bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 mb-5"
+                  onClick={AddCartConfirm}
+                >Add Cart</button>
               </div>
             )}
           </div>
